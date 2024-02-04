f65a9e972e31936004c776e9f408e1d7f2e844e7
Jazeel <avjazeel2003@gmail.com>
2024-02-05 02:48:37 +0530

    Avatars_in_Horizontal

--- lib/Union/SubCommittee/union_garden.dart
+++ lib/Union/SubCommittee/union_garden.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_111_copy/Union/SubCommittee/union_literary.dart';
 import 'package:flutter_application_111_copy/Union/SubCommittee/union_sub_committee.dart';
 
-
-void main()=> runApp(UnionGarden());
+void main() => runApp(UnionGarden());
 
 class UnionGarden extends StatelessWidget {
-  const UnionGarden({super.key});
+  const UnionGarden({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,119 +16,139 @@ class UnionGarden extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.teal,
-          title: Center(child: Text("Garden"))
-          ),
-          body: SingleChildScrollView(child: Body()),
-        ));
+          title: Center(child: Text("Garden")),
+        ),
+        body: SingleChildScrollView(child: Body()),
+      ),
+    );
   }
 }
 
 class Body extends StatefulWidget {
-  const Body({super.key});
+  const Body({Key? key}) : super(key: key);
 
   @override
   State<Body> createState() => _BodyState();
 }
 
 class _BodyState extends State<Body> {
-  List<String> Items=[
+  List<String> items = [
     'https://img.freepik.com/free-vector/christmas-background-flat-design_52683-47609.jpg?size=626&ext=jpg&ga=GA1.1.648074344.1702646045&semt=ais',
     'https://img.freepik.com/free-vector/christmas-background-with-realistic-decoration_52683-30774.jpg?size=626&ext=jpg&ga=GA1.1.648074344.1702646045&semt=ais',
     'https://img.freepik.com/free-vector/merry-christmas-wallpaper-design_79603-2129.jpg?size=626&ext=jpg&ga=GA1.1.648074344.1702646045&semt=ais',
     'https://img.freepik.com/free-vector/merry-christmas-lettering-with-pine-leaves_52683-30638.jpg?size=626&ext=jpg&ga=GA1.1.648074344.1702646045&semt=ais',
   ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Text("Notice",
-                style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  )),
+          child: Text(
+            "Notice",
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
         ),
         SizedBox(height: 10,),
-        CarouselSlider(items: Items.map((e) => ClipRRect(
-          borderRadius: BorderRadius.circular(6),
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Image.network(e,
-              height: 200,
-              width: 100,
-              fit: BoxFit.cover,
-              ),
-            ],
-          ),
-        )).toList(),
-        options: CarouselOptions(
-          autoPlay: true,
-          enableInfiniteScroll: false,
-          enlargeCenterPage: true,
-          height: 150
-        )
+        CarouselSlider(
+          items: items.map((e) => ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.network(
+                  e,
+                  height: 200,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+          )).toList(),
+          options: CarouselOptions(
+            autoPlay: true,
+            enableInfiniteScroll: false,
+            enlargeCenterPage: true,
+            height: 150
+          )
         ),
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Text("Committee Members",
-           style: TextStyle(
-            color: Colors.red,
+          child: Text(
+            "Committee Members",
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage('assets/images/me.jpg'),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Your Name',
+                  style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 20,
-           ),
-           ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage('assets/images/PSC1.jpg'),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Second Person Name',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-        Padding(
- padding: const EdgeInsets.fromLTRB(8, 20, 200, 50),
- child: CircleAvatar(
-    radius: 70,
-    backgroundImage: AssetImage('assets/images/me.jpg'),
- ),
-),
-Padding(
- padding: EdgeInsets.fromLTRB(20, 0, 200, 100),
- child: Column(
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: CircleAvatar(
-          radius: 70,
-          backgroundImage: AssetImage('assets/images/PSC1.jpg'),
-        ),
-      ),
-      ],
-    )
-    ),
-    Row(
+        SizedBox(height: 20),
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                    child: Text('Back'),
-                    onPressed: () {
-                      Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => UnionSubcommittee(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UnionSubcommittee()),
+                );
+              },
+              child: Text('Back'),
             ),
-            );
-                    },
-                    ),
+            SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UnionLiterary()),
+                );
+              },
+              child: Text('Next'),
             ),
-           ElevatedButton( 
-          child: Text('Next'),
-          onPressed: () {
-            Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => UnionLiterary(),
-            ),
-            );
-          },
-          ),
-              ],
-              ),
-    ]
+          ],
+        ),
+      ],
     );
   }
 }
