36b451217e5a208c391d3fbd48334e832b46d539
Jazeel <avjazeel2003@gmail.com>
2024-03-13 06:46:46 +0530

    Stock

--- lib/Offiice/office_stock.dart
+++ lib/Offiice/office_stock.dart
@@ -1,86 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class OfficeStockPage extends StatefulWidget {
   @override
   _OfficeStockPageState createState() => _OfficeStockPageState();
 }
 
-
-
 class _OfficeStockPageState extends State<OfficeStockPage> {
-  List<List<String>> gridData = List.generate(10, (_) => List.filled(10, ""));
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final TextEditingController _itemNameController = TextEditingController();
+  final TextEditingController _stockController = TextEditingController();
 
-  
-
-  void addRow() {
-    setState(() {
-      gridData.add(List.filled(gridData[0].length, ""));
-    });
-  }
-
-  void addColumn() {
-    setState(() {
-      for (int i = 0; i < gridData.length; i++) {
-        gridData[i].add("");
-      }
-    });
+  Future<void> _addItem(String itemName, int stock) async {
+    try {
+      await _firestore.collection('office_stock').add({
+        'item_name': itemName,
+        'stock': stock,
+      });
+    } catch (e) {
+      print('Error adding item: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Office Stock Page'),
+        title: Text('Office Stock'),
       ),
       body: Column(
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: DataTable(
-                  columns: List.generate(
-                    gridData[0].length,
-                    (index) => DataColumn(label: Text('Column $index')),
-                  ),
-                  rows: List.generate(
-                    gridData.length,
-                    (index) => DataRow(
-                      cells: List.generate(
-                        gridData[index].length,
-                        (cellIndex) => DataCell(
-                          TextFormField(
-                            initialValue: gridData[index][cellIndex],
-                            onChanged: (newValue) {
-                              setState(() {
-                                gridData[index][cellIndex] = newValue;
-                              });
-                            },
-                          ),
-                        ),
-                      ),
-                    ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _itemNameController,
+                    decoration: InputDecoration(labelText: 'Item'),
                   ),
                 ),
-              ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: TextField(
+                    controller: _stockController,
+                    decoration: InputDecoration(labelText: 'Stock'),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    _addItem(_itemNameController.text, int.parse(_stockController.text));
+                    _itemNameController.clear();
+                    _stockController.clear();
+                  },
+                  child: Text('Add Stock'),
+                ),
+              ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: addRow,
-                child: Text('Add Row'),
-              ),
-              ElevatedButton(
-                onPressed: addColumn,
-                child: Text('Add Column'),
-              ),
-            ],
+          Expanded(
+            child: StreamBuilder(
+              stream: _firestore.collection('office_stock').snapshots(),
+              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+
+                return ListView(
+                  children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                    return ListTile(
+                      title: Text(data['item_name']),
+                      subtitle: Text('Stock: ${data['stock']}'),
+                    );
+                  }).toList(),
+                );
+              },
+            ),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _itemNameController.dispose();
+    _stockController.dispose();
+    super.dispose();
   }
 }
