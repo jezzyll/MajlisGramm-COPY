d8e6663fc15c34858adfa714c5dc7c15a9d3db5e
Jazeel <avjazeel2003@gmail.com>
2024-03-16 13:59:08 +0530

    signedUpUI

--- lib/Auth/signedup_screen.dart
+++ lib/Auth/signedup_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_111_copy/Auth/signin_scree.dart';
 
 class SignedUpScreen extends StatefulWidget {
-  const SignedUpScreen({super.key});
+  const SignedUpScreen({Key? key});
 
   @override
   State<SignedUpScreen> createState() => _SignedUpScreenState();
@@ -14,36 +14,85 @@ class _SignedUpScreenState extends State<SignedUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-     String? _email = _auth.currentUser!.email;
+    String? _email = _auth.currentUser!.email;
     return Scaffold(
       appBar: AppBar(
         title: Text("You Are In"),
+        backgroundColor: Colors.green,
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Registered Successfully with: $_email"),
-              SizedBox(height: 50,),
-               ElevatedButton(
-                  onPressed: (){
-                    Navigator.push(
-                      context, MaterialPageRoute(
-                        builder: (ctx){
-                          return LoginScreen();
-                          }
-                          )
-                          );
-        
-                  }, child: Text("Sign In"),
-                  
-                  )
-            ],
-          ),),
-          
-        ),
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width,
+            bottom: 35,
+            child: Opacity(
+              opacity: 0.5,
+              child: Image.asset(
+                "assets/images/signedup.png",
+                height: 400,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Registered Successfully with: $_email",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 500),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) {
+                              return LoginScreen();
+                            },
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.green,
+                            elevation: 30,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 30,
+                            ),
+                          ),
+                      child: Text("Sign In",
+                      style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
