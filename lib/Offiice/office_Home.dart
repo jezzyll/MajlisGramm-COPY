a682398d1a80117e37c175676fb296abe342ffc9
Jazeel <avjazeel2003@gmail.com>
2024-03-16 23:09:04 +0530

    office Ui

--- lib/Offiice/office_Home.dart
+++ lib/Offiice/office_Home.dart
@@ -10,38 +10,46 @@ class OfficeHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Majlis Office'),
+        backgroundColor: Colors.green,
       ),
       
       body: GridView.count(
         crossAxisCount: 2,
         children: [
-          
-          _buildCard(context, 'OfficeStaff', '/officestaff'),
-          _buildCard(context, 'Non-Staff', '/nonStaff'),
-          _buildCard(context, 'New Admission', '/newAdmission'),
-          _buildCard(context, 'Fee Details', '/feeDetails'),
-          _buildCard(context, 'Stock', '/stock'),
+          _buildCard(context, 'Office Staff', Icons.people, '/officestaff', Colors.teal),
+          _buildCard(context, 'Non-Staff', Icons.person, '/nonStaff', Colors.teal),
+          _buildCard(context, 'New Admission', Icons.add, '/newAdmission', Colors.teal),
+          _buildCard(context, 'Fee Details', Icons.attach_money, '/feeDetails', Colors.teal),
+          _buildCard(context, 'Stock', Icons.inventory, '/stock', Colors.teal),
         ],
       ),
     );
   }
 
-  Widget _buildCard(BuildContext context, String title, String route) {
+  Widget _buildCard(BuildContext context, String title, IconData iconData, String route, Color color) {
     return GestureDetector(
       onTap: () {
         Navigator.pushNamed(context, route);
       },
       child: Card(
         margin: EdgeInsets.all(16.0),
-        child: Center(
-          child: Text(
-            title,
-            style: TextStyle(fontSize: 20.0),
-          ),
+        color: color,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              size: 50.0,
+              color: Colors.white,
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              title,
+              style: TextStyle(fontSize: 20.0, color: Colors.white),
+            ),
+          ],
         ),
       ),
     );
-    
+  }
 }
-}
-
